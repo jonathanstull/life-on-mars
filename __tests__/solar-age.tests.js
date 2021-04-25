8320203f71857a73ab9d929d576b6b252057e694
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-25 15:09:53 -0700

    write seventh test, convert years past for all planets

--- __tests__/solar-age.tests.js
+++ __tests__/solar-age.tests.js
@@ -29,40 +29,40 @@ describe('Human', () => {
     expect(jonathan.mercury.get("mercuryYearsLeft")).toEqual(191.6666);
   })
 
-  it('should calculate and convert Mercury years past from Earth years', () => {
-    jonathan.mercuryConversion();
-    expect(jonathan.yearsPast.get("mercuryYearsPast")).toEqual(-191.6666);
-  })
-
   it('should convert Mercury values to Mercury years', () => {
     jonathan.mercuryConversion();
     expect(jonathan.mercury.get("mercuryAge")).toEqual(141.6667);
     expect(jonathan.mercury.get("mercuryDeathAge")).toEqual(333.3333);
     expect(jonathan.mercury.get("mercuryYearsLeft")).toEqual(191.6666);
-    expect(jonathan.yearsPast.get("mercuryYearsPast")).toEqual(-191.6666);
   })
-
+  
   it('should set and convert Venus values to Venus years', () => {
     jonathan.venusConversion();
     expect(jonathan.venus.get("venusAge")).toEqual(54.8387);
     expect(jonathan.venus.get("venusDeathAge")).toEqual(129.0323);
     expect(jonathan.venus.get("venusYearsLeft")).toEqual(74.1936);
-    expect(jonathan.yearsPast.get("venusYearsPast")).toEqual(-74.1936);
   })
-
+  
   it('should set and convert Mars values to Mars years', () => {
     jonathan.marsConversion();
     expect(jonathan.mars.get("marsAge")).toEqual(18.0851);
     expect(jonathan.mars.get("marsDeathAge")).toEqual(42.5532);
     expect(jonathan.mars.get("marsYearsLeft")).toEqual(24.4681);
-    expect(jonathan.yearsPast.get("marsYearsPast")).toEqual(-24.4681);
   })
-
+  
   it('should set and convert Jupiter values to Jupiter years', () => {
     jonathan.jupiterConversion();
     expect(jonathan.jupiter.get("jupiterAge")).toEqual(18.0851);
     expect(jonathan.jupiter.get("jupiterDeathAge")).toEqual(42.5532);
     expect(jonathan.jupiter.get("jupiterYearsLeft")).toEqual(24.4681);
+  })
+  
+  it('should convert all years past if Earth age is greater than life expectancy', () => {
+    let jonathan = new Human(90, 80);
+    jonathan.calculateYearsPast();
+    expect(jonathan.yearsPast.get("mercuryYearsPast")).toEqual(-191.6666);
+    expect(jonathan.yearsPast.get("venusYearsPast")).toEqual(-74.1936);
+    expect(jonathan.yearsPast.get("marsYearsPast")).toEqual(-24.4681);
     expect(jonathan.yearsPast.get("jupiterYearsPast")).toEqual(-24.4681);
   })
 });
--- src/js/solar-age.js
+++ src/js/solar-age.js
@@ -10,17 +10,29 @@ export default class Human {
     this.mars = {};
     this.jupiter = {};
   }
+  
+  calculateYearsPast() {
+    // this.yearsPast = new Map();
+    // if (this.earthAge >= this.earthDeathAge) {
+    //   let mercuryYearsPast = (this.mercury.get("mercuryAge") - this.mercury.get("mercuryDeathAge")).toFixed(4);
+    //   this.yearsPast.set("mercuryYearsPast", parseFloat(mercuryYearsPast));
+    //   let venusYearsPast = (this.venus.get("venusAge") - this.venus.get("venusDeathAge")).toFixed(4);
+    //   this.venus.set("venusYearsPast", parseFloat(venusYearsPast));
+    //   let marsYearsPast = (this.mars.get("marsAge") - this.mars.get("marsDeathAge")).toFixed(4);
+    //   this.mars.set("marsYearsPast", parseFloat(marsYearsPast));
+    //   let jupiterYearsPast = (this.jupiter.get("jupiterAge") - this.jupiter.get("jupiterDeathAge")).toFixed(4);
+    //   this.jupiter.set("jupiterYearsPast", parseFloat(jupiterYearsPast));
+    // } else return;
+  }
 
   mercuryConversion() {
     this.mercury = new Map();
     let mercuryAge = (this.earthAge / .24).toFixed(4);
     let mercuryDeathAge = (this.earthDeathAge / .24).toFixed(4);
     let mercuryYearsLeft = (mercuryDeathAge - mercuryAge).toFixed(4);
-    let mercuryYearsPast = (mercuryAge - mercuryDeathAge).toFixed(4);
     this.mercury.set("mercuryAge", parseFloat(mercuryAge));
     this.mercury.set("mercuryDeathAge", parseFloat(mercuryDeathAge));
     this.mercury.set("mercuryYearsLeft", parseFloat(mercuryYearsLeft));
-    this.mercury.set("mercuryYearsPast", parseFloat(mercuryYearsPast));
   }
 
   venusConversion() {
@@ -28,11 +40,9 @@ export default class Human {
     let venusAge = (this.earthAge / .62).toFixed(4);
     let venusDeathAge = (this.earthDeathAge / .62).toFixed(4);
     let venusYearsLeft = (venusDeathAge - venusAge).toFixed(4);
-    let venusYearsPast = (venusAge - venusDeathAge).toFixed(4);
     this.venus.set("venusAge", parseFloat(venusAge));
     this.venus.set("venusDeathAge", parseFloat(venusDeathAge));
     this.venus.set("venusYearsLeft", parseFloat(venusYearsLeft));
-    this.venus.set("venusYearsPast", parseFloat(venusYearsPast));
   }
 
   marsConversion() {
@@ -40,11 +50,9 @@ export default class Human {
     let marsAge = (this.earthAge / 1.88).toFixed(4);
     let marsDeathAge = (this.earthDeathAge / 1.88).toFixed(4);
     let marsYearsLeft = (marsDeathAge - marsAge).toFixed(4);
-    let marsYearsPast = (marsAge - marsDeathAge).toFixed(4);
     this.mars.set("marsAge", parseFloat(marsAge));
     this.mars.set("marsDeathAge", parseFloat(marsDeathAge));
     this.mars.set("marsYearsLeft", parseFloat(marsYearsLeft));
-    this.mars.set("marsYearsPast", parseFloat(marsYearsPast));
   }
 
   jupiterConversion() {
@@ -52,10 +60,8 @@ export default class Human {
     let jupiterAge = (this.earthAge / 1.88).toFixed(4);
     let jupiterDeathAge = (this.earthDeathAge / 1.88).toFixed(4);
     let jupiterYearsLeft = (jupiterDeathAge - jupiterAge).toFixed(4);
-    let jupiterYearsPast = (jupiterAge - jupiterDeathAge).toFixed(4);
     this.jupiter.set("jupiterAge", parseFloat(jupiterAge));
     this.jupiter.set("jupiterDeathAge", parseFloat(jupiterDeathAge));
     this.jupiter.set("jupiterYearsLeft", parseFloat(jupiterYearsLeft));
-    this.jupiter.set("jupiterYearsPast", parseFloat(jupiterYearsPast));
   }
 }
