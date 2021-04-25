15fa377819109fb0974ca60c904c077d78f185fe
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-25 15:43:03 -0700

    add user interface logic to main.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Human from './js/solar-age.js';
 
+$(document).ready(function() {
+  $('#age-input').submit(function(event) {
+    event.preventDefault();
+    const earthAge = $('#earth-age').val();
+    const earthDeathAge = $('#life-expectancy').val();
+    let human = new Human(earthAge, earthDeathAge);
+    human.mercuryConversion();
+    human.venusConversion();
+    human.marsConversion();
+    human.jupiterConversion();
+    human.calculateYearsPast();
+  });
+});
